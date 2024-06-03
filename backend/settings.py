a8c9814080e329e6877007e0aaeb44026bff5dfb
MaxGitHam <max@programming-stage.com>
2024-06-03 06:51:55 +0200

    Update settings.py
    Changed static

--- backend/settings.py
+++ backend/settings.py
@@ -140,9 +140,11 @@ USE_TZ = True
 #STATICFILES_DIRS = [BASE_DIR, 'static']
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR / 'static']
-STATIC_ROOT= BASE_DIR / 'staticfiles'
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS=[BASE_DIR / 'static']
+#STATIC_ROOT= BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 MEDIA_URL = '/media/'
